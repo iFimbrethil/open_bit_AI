a57f9cd0601381ac3452efe175fb89dbb743bd7e
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-11-04 17:54:02 -0400

    added some code to query network via bitapai

--- backend/query_bittensor.py
+++ backend/query_bittensor.py
@@ -1,7 +1,33 @@
-import bittensor
+import http.client
+import json
+from dotenv import load_dotenv
+import os
 
-def query(context: str):
-    out = ""
-    while out == "":
-        out = bittensor.prompt(context)
-    return out
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the BITAPAI_API_KEY from the environment variables
+bitapai_key = os.getenv('BITAPAI_API_KEY')
+
+print(bitapai_key)
+
+conn = http.client.HTTPSConnection("api.bitapai.io")
+payload = json.dumps({
+  "messages": [
+    {
+      "role": "user",
+      "content": "What is the meaning of life?"
+    },
+  ],
+  "pool_id": 4,
+  "count": 5,
+  "return_all": True
+})
+headers = {
+  'Content-Type': 'application/json',
+  'X-API-KEY': str(bitapai_key),  # Use the environment variable here
+}
+conn.request("POST", "/text", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
